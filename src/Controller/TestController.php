4eb61ad663c314e509a9ba841c49be502defd929
MFElAifa <medferjani.elifa@gmail.com>
2023-07-26 18:34:24 +0200

    Ajout des fichiers de base

---
+++ src/Controller/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController
+{
+    #[Route('/test/{age<\d+>?0}', name: "test", methods:['GET', 'POST'], host: 'localhost', schemes:['http', 'https'])]
+    public function test(Request $request, $age)
+    {
+        return new Response("Vous avez $age ans !");
+    }
+}
